e1c5d4fef1135c390dc861848854ec417321c4c5
Mohammad Yazdian <m.yazdian70@outlook.com>
2023-09-26 11:41:25 +0330

    Initialize project

---
+++ main/src/main/java/ir/co/holoo/middleware/main/MiddlewareApplication.java
@@ -0,0 +1,29 @@
+package ir.co.holoo.middleware.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(
+        exclude = {
+                MessageSourceAutoConfiguration.class,
+        },
+        proxyBeanMethods = false,
+        scanBasePackages = {
+                "ir.co.holoo.middleware.controller",
+                "ir.co.holoo.middleware.persistence",
+                "ir.co.holoo.middleware.service",
+                "ir.co.holoo.middleware.main"
+        })
+@EntityScan(basePackages = {"ir.co.holoo.middleware.model"})
+@ConfigurationPropertiesScan(basePackages = {"ir.co.holoo.middleware.service.properties"})
+@EnableDiscoveryClient
+public class MiddlewareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddlewareApplication.class, args);
+    }
+}
